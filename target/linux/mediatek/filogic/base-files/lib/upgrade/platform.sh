061eb2515d0322daaef2213796ae1154df8258ba
Daniel Golle <daniel@makrotopia.org>
2025-10-05 04:23:29 +0100

    mediatek: add support for BananaPi BPi-R4 Lite
    The BPi-R4 Lite is a WiFi-7 router board based on the MT7987 SoC.
    
    Specification :
     - SOC: Mediatek MT7987A (4x Cortex-A53
     - RAM: 2GB
     - Flash: 32MB SPI NOR, 256MB SPI NAND, 8GB eMMC
     - Switch: MediaTek MT7531AE
     - Ports : 4x LAN (1G), 1x SFP (via MT7531), 1x WAN (2.5G)
     - Buttons : Reset & WPS/Mesh
     - LEDs : Status (PWM), SFP
     - USB: on-board VIA VL817 USB3.1/USB2.0 hub
        * 1 - mPCIe B (SIM3)
        * 2 - NGFF-KEYB (SIM1)
        * 3 - USB-A connector
        * 4 - mPCIe A (SIM4)
     - mPCIe: 1x 8GT/s x2 or 2x 8GT/s x1 (configurable via bootloader)
     - RTC: PCF8563
     - mikroBUS socket with SPI, I2C and full UART
     - on-board HT42B534 USB-to-serial for Type-C console port
     - Power: USB Type-C PD 20V, or DC via barrel connector or JST-VH 3.96
    
    Installation:
    Uncompress *sdcard.img.gz and write to microSD card, eg. using 'dd'.
    Use bootloader menu on the serial console to install SPI-NAND or SPI-NOR,
    once installed to SPI-NAND you can use the bootloader menu to install to
    eMMC. See instructions for BananaPi R3 for details.
    
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -86,6 +86,7 @@ platform_do_upgrade() {
 	bananapi,bpi-r4|\
 	bananapi,bpi-r4-2g5|\
 	bananapi,bpi-r4-poe|\
+	bananapi,bpi-r4-lite|\
 	cmcc,a10-ubootmod|\
 	cmcc,rax3000m|\
 	cudy,tr3000-v1-ubootmod|\
@@ -253,6 +254,7 @@ platform_check_image() {
 	bananapi,bpi-r4|\
 	bananapi,bpi-r4-2g5|\
 	bananapi,bpi-r4-poe|\
+	bananapi,bpi-r4-lite|\
 	cmcc,a10-ubootmod|\
 	cmcc,rax3000m|\
 	cudy,tr3000-v1-ubootmod|\
@@ -309,6 +311,7 @@ platform_copy_config() {
 	bananapi,bpi-r4|\
 	bananapi,bpi-r4-2g5|\
 	bananapi,bpi-r4-poe|\
+	bananapi,bpi-r4-lite|\
 	cmcc,rax3000m|\
 	gatonetworks,gdsp|\
 	mediatek,mt7988a-rfb)
